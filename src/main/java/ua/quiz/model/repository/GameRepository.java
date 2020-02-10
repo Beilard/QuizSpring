f9d4275c053c8991ac7e92d613abfb1090cd9619
Ferox <gipnous@gmail.com>
2020-02-10 06:06:05 +0200

    Merge.

--- src/main/java/ua/quiz/model/repository/GameRepository.java
+++ src/main/java/ua/quiz/model/repository/GameRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.quiz.model.entity.GameEntity;
 
-
 public interface GameRepository extends JpaRepository<GameEntity, Long> {
     Page<GameEntity> findAll(Pageable pageable);
 
--- src/main/java/ua/quiz/model/repository/QuestionRepository.java
+++ src/main/java/ua/quiz/model/repository/QuestionRepository.java
@@ -3,7 +3,6 @@ package ua.quiz.model.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.quiz.model.entity.QuestionEntity;
 
-
 public interface QuestionRepository extends JpaRepository<QuestionEntity, Long> {
 
 }
